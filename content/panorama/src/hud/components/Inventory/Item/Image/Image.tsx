8c38363a66d72ec81142530d7da2122a95729773
szBluebird <e.thoalm@gmail.com>
2021-12-16 17:09:52 +0100

    stuff

--- content/panorama/src/hud/components/Inventory/Item/Image/Image.tsx
+++ content/panorama/src/hud/components/Inventory/Item/Image/Image.tsx
@@ -15,7 +15,7 @@ const Image = (props: Props) => {
   const { item, selectedUnit } = props;
 
   const [isCooldownReady, setIsCooldownReady] = useState(Abilities.IsCooldownReady(item));
-  const [isActive, setIsActive] = useState(Abilities.IsActivated(item));
+  const [isActive, setIsActive] = useState(false);
   const [hasEnoughMana, setHasEnoughMana] = useState(Abilities.IsOwnersManaEnough(item));
   const [texture, setTexutre] = useState(Abilities.GetAbilityTextureName(item));
   const [showLock, setShowLock] = useState(false);
@@ -38,7 +38,7 @@ const Image = (props: Props) => {
       className={Styles.container}
       style={{
         backgroundColor: isActive
-          ? "gradient(linear, 50% 0%, 50% 40%, from(rgba(200, 200, 200, 0.5)), color-stop(0.5, #045d5688), to(rgb(0, 0, 0)) );"
+          ? "gradient(linear, 50% 0%, 50% 50%, from(rgba(255, 255, 255, 0.5)), color-stop(0.5, #045d5688), to(rgb(0, 0, 0)) );"
           : "black"
       }}
     >
@@ -51,7 +51,7 @@ const Image = (props: Props) => {
         style={{
           saturation: isCooldownReady ? '1.0' : '0.5',
           washColor: (showLock || !isCooldownReady) ? 'rgba(100, 100, 100, 0.8)' : hasEnoughMana ? 'none' : '#1569be',
-          padding: isActive ? '-0.75px' : '0px',
+          padding: isActive ? '-0.75px' : !isCooldownReady ? '1px' : '0px',
         }}
       />
     </Panel>
