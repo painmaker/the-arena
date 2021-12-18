ef7858bced4451f978f4a81b6b3fc465b49c19c4
szBluebird <e.thoalm@gmail.com>
2021-12-18 23:51:57 +0100

    stuff

--- game/scripts/vscripts/abilities/heroes/dazzle/dazzle_depraved_healing.ts
+++ game/scripts/vscripts/abilities/heroes/dazzle/dazzle_depraved_healing.ts
@@ -1,14 +1,63 @@
 import { BaseAbility, registerAbility } from "../../../lib/dota_ts_adapter";
 
+const SOUNDS = [
+  "dazzle_dazz_pain_01",
+  "dazzle_dazz_pain_02",
+  "dazzle_dazz_pain_03",
+  "dazzle_dazz_pain_04",
+  "dazzle_dazz_pain_05",
+  "dazzle_dazz_pain_06",
+  "dazzle_dazz_pain_07",
+  "dazzle_dazz_pain_08",
+  "dazzle_dazz_pain_09",
+  "dazzle_dazz_pain_10",
+  "dazzle_dazz_pain_11",
+  "dazzle_dazz_pain_12",
+]
 @registerAbility()
 export class dazzle_depraved_healing extends BaseAbility {
 
-  OnSpellStart(): void {
-    print("Spell Started")
+  CastFilterResultTarget(target: CDOTA_BaseNPC): UnitFilterResult {
+    if (IsServer()) {
+      const damage = this.GetSpecialValueFor("damage");
+      const heal = this.GetSpecialValueFor("heal");
+      const caster = this.GetCaster();
+      const health = caster.GetHealth();
+      const threshold = caster === target ? health + heal : health;
+      if (damage >= threshold) {
+        return UnitFilterResult.UF_FAIL_CUSTOM;
+      }
+    }
+    return UnitFilterResult.UF_SUCCESS;
   }
 
-  GetCastRange(location: Vector, target: CDOTA_BaseNPC | undefined): number {
-    return this.GetSpecialValueFor("cast_range") + this.GetCaster().GetCastRangeBonus();
+  GetCustomCastErrorTarget(): string {
+    return "#dota_hud_error_health_too_low";
+  }
+
+  OnSpellStart(): void {
+
+    const heal = this.GetSpecialValueFor('heal');
+    const damage = this.GetSpecialValueFor('damage');
+
+    const caster = this.GetCaster();
+    const target = this.GetCursorTarget()!;
+
+    caster.EmitSoundParams(SOUNDS[RandomInt(0, SOUNDS.length - 1)], 2.2, 2.2, 0);
+    caster.EmitSoundParams("Hero_Dazzle.Poison_Cast", 1.0, 1.0, 0);
+    caster.EmitSoundParams("hero_bloodseeker.attack", 0.75, 0.75, 0);
+
+    target.Heal(heal, this);
+
+    ApplyDamage({
+      victim: caster,
+      attacker: caster,
+      damage: damage,
+      damage_type: this.GetAbilityDamageType(),
+      damage_flags: DOTADamageFlag_t.DOTA_DAMAGE_FLAG_NON_LETHAL,
+      ability: this,
+    });
+
   }
 
 }
